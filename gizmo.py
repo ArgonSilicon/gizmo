f9f9241c55929d9f86c9e5662d4440e288a049e7
ArgonSilicon <arsi.ikaheimonen@gmail.com>
2021-04-13 13:14:07 +0300

    exercises 2-4

--- gizmo.py
+++ gizmo.py
@@ -5,3 +5,14 @@ Created on Mon Apr 13 12:26:42 2021
 
 @author: arsii
 """
+
+def hello(name, country='Finland'):
+	print(f'Hello {name}, how are things in {country}?')
+	
+def spell(word):
+    for i in word:
+        print(i, end='.')
+    print("\n")
+    	
+def relative_path(subjects):
+    return [f'./subjects/mock_recording_{i}.rec' for i in subjects]
